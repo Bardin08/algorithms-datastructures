2201d1e5e6604fc46497dc4b7ea252ef205cf4c6
Bardin08 <vbardin810@gmail.com>
2024-01-16 00:04:29 +0200

    Refactor tests

--- ShuntingYard.Tests/TokenizationProcessorTests.cs
+++ ShuntingYard.Tests/TokenizationProcessorTests.cs
@@ -2,12 +2,13 @@ namespace ShuntingYard.Tests;
 
 public class TokenizationProcessorTests
 {
+    private readonly TokenizationProcessor _sut = new();
+    
     [Theory]
     [InlineData("10+3", "10", "+", "3")]
     public void Positive_IntNumbers_Success(string input, params string[] expectedTokens)
     {
-        var tokenizer = new TokenizationProcessor();
-        var actualTokens = tokenizer.Tokenize(input);
+        var actualTokens = _sut.Tokenize(input);
         Assert.Equivalent(expectedTokens, actualTokens);
     }
 
@@ -15,8 +16,7 @@ public class TokenizationProcessorTests
     [InlineData("-10+-3", "-10", "+", "-3")]
     public void Negative_IntNumbers_Success(string input, params string[] expectedTokens)
     {
-        var tokenizer = new TokenizationProcessor();
-        var actualTokens = tokenizer.Tokenize(input);
+        var actualTokens = _sut.Tokenize(input);
         Assert.Equivalent(expectedTokens, actualTokens);
     }
 
@@ -24,8 +24,7 @@ public class TokenizationProcessorTests
     [InlineData("10+-3", "10", "+", "-3")]
     public void PositiveAndNegative_IntNumbers_Success(string input, params string[] expectedTokens)
     {
-        var tokenizer = new TokenizationProcessor();
-        var actualTokens = tokenizer.Tokenize(input);
+        var actualTokens = _sut.Tokenize(input);
         Assert.Equivalent(expectedTokens, actualTokens);
     }
 
@@ -33,8 +32,7 @@ public class TokenizationProcessorTests
     [InlineData("10.65+3.65", "10.65", "+", "3.65")]
     public void Positive_DoubleNumbers_Success(string input, params string[] expectedTokens)
     {
-        var tokenizer = new TokenizationProcessor();
-        var actualTokens = tokenizer.Tokenize(input);
+        var actualTokens = _sut.Tokenize(input);
         Assert.Equivalent(expectedTokens, actualTokens);
     }
 
@@ -42,8 +40,7 @@ public class TokenizationProcessorTests
     [InlineData("-10.65+-3.65", "-10.65", "+", "-3.65")]
     public void Negative_DoubleNumbers_Success(string input, params string[] expectedTokens)
     {
-        var tokenizer = new TokenizationProcessor();
-        var actualTokens = tokenizer.Tokenize(input);
+        var actualTokens = _sut.Tokenize(input);
         Assert.Equivalent(expectedTokens, actualTokens);
     }
 
@@ -51,8 +48,23 @@ public class TokenizationProcessorTests
     [InlineData("10.65+-3.22", "10.65", "+", "-3.22")]
     public void PositiveAndNegative_DoubleNumbers_Success(string input, params string[] expectedTokens)
     {
-        var tokenizer = new TokenizationProcessor();
-        var actualTokens = tokenizer.Tokenize(input);
+        var actualTokens = _sut.Tokenize(input);
         Assert.Equivalent(expectedTokens, actualTokens);
+    }
+
+    [Theory]
+    [InlineData("10.65+-3.22", "10.65", "+", "-3.22")]
+    public void DoubleNumbers_DecimalPointValidation_Success(string input, params string[] expectedTokens)
+    {
+        var actualTokens = _sut.Tokenize(input);
+        Assert.Equivalent(expectedTokens, actualTokens);
+    }
+
+    [Theory]
+    [InlineData("10.6.5+-3.22")]
+    public void DoubleNumbers_DecimalPointValidation_Failed(string input)
+    {
+        Action action = () => _sut.Tokenize(input);
+        Assert.Throws<Exception>(action);
     }
 }
