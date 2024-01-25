f6b649036412b50b39e2f1cb295e5bc37fb13474
Bardin08 <vbardin810@gmail.com>
2024-01-26 01:33:58 +0200

    Add linked-list based queue

---
+++ DataStructures/Queue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace DataStructures;
+
+internal class Queue : IEnumerable
+{
+    private Node? _head;
+    private Node? _tail;
+
+    public int Count { get; private set; }
+
+    public void Enqueue(string value)
+    {
+        var node = new Node
+        {
+            Value = value
+        };
+
+        if (_tail != null)
+        {
+            _tail.Next = node;
+        }
+
+        if (_head == null &&
+            _tail == null)
+        {
+            _head = _tail = node;
+        }
+
+        _tail = node;
+        Count++;
+    }
+
+    public string? Dequeue()
+    {
+        var head = _head;
+        _head = _head?.Next;
+
+        Count--;
+        return head?.Value;
+    }
+
+    public string? Peek()
+    {
+        return _head?.Value;
+    }
+
+    public void Clear()
+    {
+        _head = _tail = null;
+        Count = 0;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        while (_head != null)
+        {
+            var head = _head;
+            _head = _head.Next;
+
+            yield return head.Value;
+        }
+    }
+}
