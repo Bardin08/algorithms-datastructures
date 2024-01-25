0d006adffc95ff21775e3bd9ed6ce9d40e3618be
Bardin08 <vbardin810@gmail.com>
2024-01-26 01:44:00 +0200

    Add queues tests implementation

---
+++ DataStructures.Tests/ArrayQueueTests.cs
@@ -0,0 +1,48 @@
+﻿namespace DataStructures.Tests;
+
+public class ArrayQueueTests
+{
+    [Fact]
+    public void Enqueue_ThreeElements_Valid()
+    {
+        var queue = new ArrayQueue<string>();
+
+        queue.Enqueue("1");
+        queue.Enqueue("2");
+        queue.Enqueue("3");
+
+        Assert.Equal(3, queue.Count);
+    }
+
+    [Fact]
+    public void Dequeue_ThreeElements_Valid()
+    {
+        var queue = new ArrayQueue<string>();
+        queue.Enqueue("1");
+        queue.Enqueue("2");
+        queue.Enqueue("3");
+
+        queue.Dequeue();
+        queue.Dequeue();
+        queue.Dequeue();
+
+        Assert.Equal(0, queue.Count);
+    }
+
+    [Fact]
+    public void Peek_ThreeElements_Valid()
+    {
+        var queue = new ArrayQueue<string>();
+        queue.Enqueue("1");
+        queue.Enqueue("2");
+        queue.Enqueue("3");
+
+        Assert.Equal(3, queue.Count);
+
+        queue.Peek();
+        queue.Peek();
+        queue.Peek();
+
+        Assert.Equal(3, queue.Count);
+    }
+}
