3403a9cf3d2b2ea6d296aa916930a051ee86c774
Bardin08 <vbardin810@gmail.com>
2024-01-26 01:34:27 +0200

    Add Program.cs

---
+++ DataStructures/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataStructures;
+
+var queue = new ArrayQueue<string>();
+
+queue.Enqueue("1");
+queue.Enqueue("2");
+queue.Enqueue("3");
+
+Console.WriteLine("Count before peek: " + queue.Count);
+
+Console.WriteLine(queue.Peek());
+Console.WriteLine(queue.Peek());
+
+Console.WriteLine("Count before dequeue: " + queue.Count);
+
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+
+Console.WriteLine("Count after dequeue: " + queue.Count);
