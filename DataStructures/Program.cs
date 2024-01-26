9f05f55044004d22d96f6302bf5e534a17668933
Bardin08 <vbardin810@gmail.com>
2024-01-26 02:39:50 +0200

    Use Program.cs class instead of top level statements

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,25 +1,29 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using DataStructures;
 
-var queue = new ArrayQueue<string>();
-
-queue.Enqueue("1");
-queue.Enqueue("2");
-queue.Enqueue("3");
-
-Console.WriteLine("Count before peek: " + queue.Count);
-
-Console.WriteLine(queue.Peek());
-Console.WriteLine(queue.Peek());
-
-Console.WriteLine("Count before dequeue: " + queue.Count);
-
-Console.WriteLine(queue.Dequeue());
-Console.WriteLine(queue.Dequeue());
-Console.WriteLine(queue.Dequeue());
-
-Console.WriteLine("Count after dequeue: " + queue.Count);
-
+namespace DataStructures;
 
 [ExcludeFromCodeCoverage]
-public partial class Program { }
+internal static partial class Program
+{
+    public static void Main()
+    {
+        var queue = new ArrayQueue<string>();
+
+        queue.Enqueue("1");
+        queue.Enqueue("2");
+        queue.Enqueue("3");
+
+        Console.WriteLine("Count before peek: " + queue.Count);
+
+        Console.WriteLine(queue.Peek());
+        Console.WriteLine(queue.Peek());
+
+        Console.WriteLine("Count before dequeue: " + queue.Count);
+
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+
+        Console.WriteLine("Count after dequeue: " + queue.Count);
+    }
+}
